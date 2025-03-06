889f07ae0f475c534a9f5d100da84638320a99ca
Dohy1080 <huydan473@gmail.com>
2025-03-06 22:07:25 +0700

    Add files via upload

---
+++ CoreEntities/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreEntities.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Column("id")]
+        public Guid Id { get; set; }
+        [Column("name")]
+        public string? Name { get; set; }
+        [Column("gender")]
+        public string? Gender { get; set; }
+        [Column("mail")]
+        public string? Mail { get; set; }
+        [Column("phonenumber")]
+        public string? PhoneNumber { get; set; }
+        [Column("birthdate")]
+        public DateTime BirthDate { get; set; }
+        [Column("image")]
+        public required string Image { get; set; }
+        [Column("firstlogin")]
+        public DateTime FirstLogin { get; set; }
+        [Column("subjectid")]
+        public Guid Subject { get; set; }
+        [Column("studentcode")]
+        public string? StudentCode { get; set; }
+    }
+}
---
+++ CoreEntities/Models/Teacher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreEntities.Models
+{
+     [Table("Teacher")]
+     public class Teacher
+     {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+        [Column("email")]
+        public string Email { get; set; }
+        [Column("phonenumber")]
+        public string? PhoneNumber { get; set; }   
+        [Column("subjectid")]
+        public int SubjectId { get; set; }
+        [Column("birthdate")]
+        public DateTime BirthDate { get; set; }
+
+
+
+    }
+}
